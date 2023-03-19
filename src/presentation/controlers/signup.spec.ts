3a228baf3aab5f9abfcc316a83bc5f44a87f571f
rodrigo-marcolino <rodrigomarcolino@gmail.com>
2023-03-19 22:02:57 +1300

    test: ensure SignUpController retuns 400 if no email is provided

--- src/presentation/controlers/signup.spec.ts
+++ src/presentation/controlers/signup.spec.ts
@@ -14,4 +14,19 @@ describe('SignUp Controler', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
+
+    test('Should return 400 if no email is provided', () => {
+      const sut = new SignUpController()
+      const httpRequest = {
+        body: {
+          name: 'my_name',
+          // email: 'my_email',
+          password: 'my_password',
+          passwordConfirmation: 'my_password',
+        },
+      }
+      const httpResponse = sut.handle(httpRequest)
+      expect(httpResponse.statusCode).toBe(400)
+      expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+    })
 })
