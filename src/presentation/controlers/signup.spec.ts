2e75c53225755634f76574d3a7ae29ac82243009
rodrigo-marcolino <rodrigomarcolino@gmail.com>
2023-03-29 21:20:20 +1300

    refactor: move sut creation to a factory helper method

--- src/presentation/controlers/signup.spec.ts
+++ src/presentation/controlers/signup.spec.ts
@@ -1,9 +1,12 @@
 import { SignUpController } from './signup'
-import {  } from "../erros/missin";
+import { MissingParamError } from '../errors/missing-param-error'
 
+const makeSut = (): SignUpController => {
+  return new SignUpController()
+}
 describe('SignUp Controler', () => {
   test('Should return 400 if no name is provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         //name: 'my_name',
@@ -14,21 +17,21 @@ describe('SignUp Controler', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+    expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
 
-    test('Should return 400 if no email is provided', () => {
-      const sut = new SignUpController()
-      const httpRequest = {
-        body: {
-          name: 'my_name',
-          // email: 'my_email',
-          password: 'my_password',
-          passwordConfirmation: 'my_password',
-        },
-      }
-      const httpResponse = sut.handle(httpRequest)
-      expect(httpResponse.statusCode).toBe(400)
-      expect(httpResponse.body).toEqual(new Error('Missing param: email'))
-    })
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'my_name',
+        // email: 'my_email',
+        password: 'my_password',
+        passwordConfirmation: 'my_password',
+      },
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('email'))
+  })
 })
